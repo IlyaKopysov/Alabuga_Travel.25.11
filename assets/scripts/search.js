2a066515d39e2406926cb9957e6c1f99ca7336ed
IlyaKopysov <ilya.kopysov@internet.ru>
2024-11-22 17:29:04 +0300

    добавление к воторой партии поиска, создание третьей партии карточек

--- assets/scripts/search.js
+++ assets/scripts/search.js
@@ -1,14 +1,27 @@
 document.addEventListener('DOMContentLoaded', async function () {
-    const content = document.querySelector('.card__search');
-    const itemsPerPage = 4;
-    let currentPage = 0;
+    const searchInput = document.getElementById('search-input');
+    const searchList = document.getElementById('search-list');
     let items = [];
-  
         const response = await fetch('https://673611ff5995834c8a954d48.mockapi.io/tasks');
         const data = await response.json();
-        console.log(data);
-        data.forEach(item => {
-            content.innerHTML += `
+        items = data;
+
+        displayItems(items);
+        searchInput.addEventListener('input', () => {
+            const searchTerm = searchInput.value.toLowerCase();
+            const filteredItems = items.filter(item => {
+                return (
+                    item.title.toLowerCase().includes(searchTerm) ||
+                    item.text.toLowerCase().includes(searchTerm) ||
+                    item.addres.toLowerCase().includes(searchTerm)
+                );
+            });
+            displayItems(filteredItems);
+        });
+    function displayItems(itemsToDisplay) {
+        searchList.innerHTML = '';
+        itemsToDisplay.forEach(item => {
+            const cardHTML = `
               <section class="card__pag">
                   <div class="card__card">
                       <div class="card__card-block">
@@ -18,8 +31,9 @@ document.addEventListener('DOMContentLoaded', async function () {
                       <p class="card__card-txt">${item.text}</p>
                       <p class="card__card-add">${item.addres}</p>
                   </div>
-              </section>`;
+              </section>
+            `;
+            searchList.innerHTML += cardHTML;
         });
-        items = Array.from(content.querySelectorAll('.card__pag'));
-        showPage(currentPage);
-  });
+    }
+});
